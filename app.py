7578959c16c0a40341e66e27ec31e66c8bed1e6b
rishikanaujia <python.institute.llp@gmail.com>
2024-12-18 17:25:19 +0530

    implement and integrate configs

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, Response
 import json
 import numpy as np
 import pandas as pd
+import configparser
+
 
 # Define a custom JSONEncoder to handle Numpy and Pandas objects
 class AdvancedJSONEncoder(json.JSONEncoder):
@@ -20,7 +22,29 @@ class AdvancedJSONEncoder(json.JSONEncoder):
             return obj.to_dict(orient='records')  # Convert to list of dictionaries
         return super().default(obj)
 
+
+# Function to load configuration from an INI file
+def config_from_ini(app, filename='configs/app_config.ini'):
+    config = configparser.ConfigParser()
+    config.read(filename)
+
+    # Load general configurations from the 'general' section of the .ini file
+    app.config['DEBUG'] = config.getboolean('general', 'DEBUG')
+    app.config['SECRET_KEY'] = config.get('general', 'SECRET_KEY')
+
+
+# Create Flask app
 app = Flask(__name__)
+
+# Load configuration from the INI file
+config_from_ini(app)
+
+@app.route('/')
+def home():
+    response_data = {'api':'home of application'}
+    response_json = json.dumps(response_data, cls=AdvancedJSONEncoder)
+    return Response(response_json , content_type='application/json')
+
 
 @app.route('/data')
 def get_data():
@@ -40,5 +64,7 @@ def get_data():
     response_json = json.dumps(response_data, cls=AdvancedJSONEncoder)
     return Response(response_json, content_type='application/json')
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Run the Flask application with the configuration loaded from the INI file
+    app.run(debug=app.config['DEBUG'])
